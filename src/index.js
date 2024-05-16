72c94c4b5c0ae032485392dd89840af1cd28c3c4
Hector Macassa <cbmacassa@gmail.com>
2024-05-16 09:09:12 +0200

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseApp = initializeApp({
+    apiKey: "AIzaSyBjiTOe4DBpAaagIBFH8YRIJMAvFP4q_mw",
+    authDomain: "alpha-note-274ac.firebaseapp.com",
+    projectId: "alpha-note-274ac",
+    storageBucket: "alpha-note-274ac.appspot.com",
+    messagingSenderId: "683733177535",
+    appId: "1:683733177535:web:3a3312c51ab980863ce20a",
+    measurementId: "G-TV6C3Q16HT"
+});
